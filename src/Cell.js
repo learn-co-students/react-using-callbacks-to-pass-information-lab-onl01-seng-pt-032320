5c84c03b4e128b4b4a94e4515ec46dc6f2b30b18
sandienunez <github email address>
2020-12-20 17:06:20 -0500

    Done.

--- src/Cell.js
+++ src/Cell.js
@@ -4,14 +4,17 @@ export default class Cell extends Component {
   
   constructor(props) {
     super(props)
+           {/* color prop = used to set initial state of cell comp. and get a state value we can change below*/}
     this.state = {
       color: this.props.color
     }
   }
+
   
   render() {
     return (
       <div className="cell" style={{backgroundColor: this.state.color}}>
+      {/* here we use the state value = (set above) to change the background color of the returned div  */}
       </div>
     )
   }
